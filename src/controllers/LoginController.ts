8bcd36e0fbf480cfe719e9f82b3d9bf67414a836
Aman07a <aman16.aa17@gmail.com>
2023-04-04 08:42:03 +0200

    Formatting

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -6,17 +6,17 @@ class LoginController {
   @get("/login")
   getLogin(req: Request, res: Response): void {
     res.send(`
-        <form method="POST">
-            <div>
-                <label>Email</label>
-                <input name="email" />
-            </div>
-            <div>
-                <label>Password</label>
-                <input name="password" type="password" />
-            </div>
-            <button>Submit</button>
-        </form>
+      <form method="POST">
+          <div>
+              <label>Email</label>
+              <input name="email" />
+          </div>
+          <div>
+              <label>Password</label>
+              <input name="password" type="password" />
+          </div>
+          <button>Submit</button>
+      </form>
     `);
   }
 }
