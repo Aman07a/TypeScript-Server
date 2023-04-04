d1019217fb83c8876ca211434bdd2bf2d2e6c059
Aman07a <aman16.aa17@gmail.com>
2023-04-04 09:26:18 +0200

    Fixing Routes

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -7,15 +7,15 @@ class LoginController {
   getLogin(req: Request, res: Response): void {
     res.send(`
       <form method="POST">
-          <div>
-              <label>Email</label>
-              <input name="email" />
-          </div>
-          <div>
-              <label>Password</label>
-              <input name="password" type="password" />
-          </div>
-          <button>Submit</button>
+        <div>
+          <label>Email</label>
+          <input name="email" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input name="password" type="password" />
+        </div>
+        <button>Submit</button>
       </form>
     `);
   }
@@ -31,5 +31,11 @@ class LoginController {
     } else {
       res.send("Invalid email or password");
     }
+  }
+
+  @get("/logout")
+  getLogout(req: Request, res: Response) {
+    req.session = undefined;
+    res.redirect("/");
   }
 }
